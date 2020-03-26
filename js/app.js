d4800e7591a55df871cca2060c02a974fc70dd92
Matthew Lopez <lopezmatt6891@gmail.com>
2020-03-25 18:09:09 -0600

    Add fourth section to html

--- js/app.js
+++ js/app.js
@@ -16,9 +16,8 @@
 /**
  * Global Variables */
 const sectionItems = document.getElementsByTagName("section");
-// Items variable is node list to loop over for future appending 
+// sectionItems variable is node list to loop over for future appending 
 // to navbar unordered list.
-const list = getElementById("navbar__list");
 
 /**
  * End Global Variables
@@ -37,31 +36,29 @@ const list = getElementById("navbar__list");
 //loop over list and append each item as child
 function buildNav() {
     for (let sectionItem of sectionItems) {    
-        let node = document.createElement("LI");
-        node.classList.toggle("menu__link");
+        let node = document.createElement("li");
+        node.className = "menu__link";
         node.innerHTML = `<a href="#${sectionItem.id}">
-        ${sectionItem.dataset.nav}</a>`;
-        node.appendChild(a);                 
-        // Add complete LI to UL aka ID navbar__list
-            // How to make sure the scrollTo function supercedes this jump to anchor?
-            // Get rid of the anchor and leave the "click" event?
-        getElementById("navbar__list").appendChild(node); 
+        ${sectionItem.dataset.nav}</a>`;           
+        document.getElementById("navbar__list").appendChild(node); 
     }
 }
 
 // Add class 'active' to section when near top of viewport
 function makeActive() {
     for (let item of sectionItems) {
-        let rect = item.getBoundingClientRect();        
+        let rect = item.getBoundingClientRect();     
         if (rect.top <= 100) {
-            item.classList.toggle("your-active-class");
-        }    
-    }    
-}
+            item.className = "your-active-class";
+        } else {
+            item.className = "";
+        }   
+    }
+}    
 
 // Scroll to anchor ID using scrollTO event
 function scrollTo(e) {
-    if (e.target && e.target.nodeName === "LI") {
+    if (e.target && e.target.nodeName === "li") {
         let li = e.target.a;
         let linknav = li.href;
         linknav.scrollIntoView();
@@ -74,10 +71,10 @@ function scrollTo(e) {
  */
 
 // Build menu
-window.onload = buildNav;
+window.onload = function() {buildNav()};
 
 // Scroll to section on link click
 document.getElementById("navbar__list").addEventListener("click", scrollTo);
 
 // Set sections as active
-window.onscroll = makeActive;
+window.onscroll = function() {makeActive()};
